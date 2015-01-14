c608b9018335b91086eae3427c57b3dcf5ffc80c
Apoorv Saini <apoorvsaini9@gmail.com>
2015-01-15 00:47:59 +0900

    Adding Structure

---
+++ botlang.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once ("config/config.php");
+/**
+* Main BotLang class
+*/
+class BotLang 
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+	public function load($code)
+	{
+
+	}
+}
+?>
